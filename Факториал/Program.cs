f76e76496f6f1229dc5eb2b289c8fad7fd7dc97a
PequnaMuerte <71525862+PequnaMuerte@users.noreply.github.com>
2020-10-04 18:14:44 +0300

    Add files via upload

---
+++ Факториал/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Факториал
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, для нахождения факториала: ");
+            int f = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+            
+            
+            for (int i=2; i<=f; i++)
+            {
+
+                result *= i;
+
+            }
+            Console.WriteLine("Результат равен: " + result);
+        }
+    }
+}
